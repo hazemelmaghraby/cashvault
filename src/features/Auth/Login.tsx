4b90851e1d223711d09e5a4e416442c25e3855fe
Hazem Muhammed El-Maghraby <hazemelmaghraby.work@gmail.com>
2026-08-24 17:30:25 +0300

    Update Login.tsx

--- src/features/Auth/Login.tsx
+++ src/features/Auth/Login.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useEffectEvent } from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Input from "../../components/ui/Input";
 import Button from "../../components/ui/Button";
